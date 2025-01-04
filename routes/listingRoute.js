9bf5648803f22beb1bcec670fc856c8520b1d260
Misheel31 <misheelrai7@gmail.com>
2025-01-04 21:34:59 +0545

    listingRoute

--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -1,50 +1,19 @@
-// const express = require("express");
-// const multer = require("multer");
-// const {
-//   createListing,
-//   getListingDetails,
-//   getListings,
-//   getListingsBySearch,
-// } = require("../controller/listingController");
-
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage });
-
-// const router = express.Router();
-
-// router.post("/create", upload.array("listingPhotos"), createListing);
-// router.get("/", getListings);
-// router.get("/:listingId", getListingDetails);
-// router.get("/search/:search", getListingsBySearch);
-
-// module.exports = router; 
-
-
-
-// const express = require("express");
-// const { createListing } = require("../controller/listingController");
-
-// const router = express.Router();
-
-// router.post("/create", createListing);
-
-// module.exports = router;
-
-
 const express = require("express");
 const { createListing } = require("../controller/listingController");
+const Listing = require("../models/listingModel"); 
+
 
 const router = express.Router();
 
 router.post("/create", createListing);
+router.get("/", async (req, res, next) => {
+    try {
+      const listings = await Listing.find();  
+      res.status(200).json(listings); 
+    } catch (error) {
+      next(error);
+    }
+  });
+  
 
 module.exports = router;
