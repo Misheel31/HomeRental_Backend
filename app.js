7f172336fadef3d020e2edac32a6f786b7435f3c
Misheel31 <misheelrai7@gmail.com>
2025-01-10 11:02:14 +0545

    app js

--- app.js
+++ app.js
@@ -1,57 +1,28 @@
-// // const express = require("express");
-// // const connectDB = require("./config/db");
-// // const app = express();
-// // const userRoute = require("./routes/userRoute");
-// // const listingRoute = require("./routes/listingRoute");
-
-// // connectDB();
-
-// // app.use(express.json());
-
-// // app.use("/api/user", userRoute);
-// // app.use("/api/listing", listingRoute); // Use the listing route
-
-// // const port = 3001;
-// // app.listen(port, () => {
-// //   console.log(`Server running at http://localhost:${port}`);
-// // });
-
-
-// const express = require ('express');
-// const listingRoutes = require ('./routes/listingRoute.js');
-// const userRoute = require ('./routes/userRoute.js')
-// const connectDB = require("./config/db");
-
-// connectDB();
-
-
-// const app = express();
-
-// app.use(express.json());
-// app.use("/api/user", userRoute);
-// app.use('/api/listings', listingRoutes);
-
-// app.listen(3000, () => console.log('Server running on port 3000'));
-
-
-const express = require('express');
-const listingRoutes = require('./routes/listingRoute.js');
-const bookingRoute = require('./routes/bookingRoute.js')
-const userRoute = require('./routes/userRoute.js');
-const authRoutes =require('./routes/authRoute.js')
-const wishlistRoutes = require('./routes/wishlistRoute.js')
+const express = require("express");
+const cors = require("cors");
+const listingRoutes = require("./routes/listingRoute.js");
+const bookingRoute = require("./routes/bookingRoute.js");
+const userRoute = require("./routes/userRoute.js");
+const authRoutes = require("./routes/authRoute.js");
+const wishlistRoutes = require("./routes/wishlistRoute.js");
+const propertyRoute = require("./routes/propertyRoute.js");
 const connectDB = require("./config/db");
+// const path = require('path');
+
+require("dotenv").config();
 
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/user", userRoute);
-app.use('/api/listings', listingRoutes);
-app.use('/api/booking', bookingRoute)
-app.use('/api/auth', authRoutes)
-app.use('/api/wishlist', wishlistRoutes)
+app.use("/api/listings", listingRoutes);
+app.use("/api/booking", bookingRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/wishlist", wishlistRoutes);
+app.use("/api/property", propertyRoute);
+app.use("/property_images", express.static("property_images"));
 
-
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
