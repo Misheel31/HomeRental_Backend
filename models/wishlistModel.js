1e5a64edaaae23b5b08aba9ada35f18984b58770
Misheel31 <misheelrai7@gmail.com>
2025-02-16 19:10:27 +0545

    Property images

--- models/wishlistModel.js
+++ models/wishlistModel.js
@@ -1,21 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const wishlistSchema = new mongoose.Schema({
-    category: String,
-    city: String,
-    state: String,
-    country: String,
-    guestCount: Number,
-    bedroomCount: Number,
-    bedCount: Number,
-    bathroomCount: Number,
-    amenities: [String],
-    listingPhotoPaths: [String],
-    title: String,
-    description: String,
-    price: String,
+  category: String,
+  city: String,
+  state: String,
+  country: String,
+  guestCount: Number,
+  bedroomCount: Number,
+  bedCount: Number,
+  bathroomCount: Number,
+  title: String,
+  description: String,
+  price: String,
+  imageURL: { type: String, required: true },
+  pricePerNight: { type: String, required: true },
+  location: { type: String, required: true },
+  // username: {
+  //   type: String,
+  //   ref: "User",
+  // },
+
+  // propertyId: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   required: true,
+  // },
 });
 
-const Wishlist = mongoose.model('wishlist', wishlistSchema);
+const Wishlist = mongoose.model("wishlist", wishlistSchema);
 
 module.exports = Wishlist;
